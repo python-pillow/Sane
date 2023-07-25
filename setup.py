0ce8895d11c2b4c2f38f2f87170dce73853d4ecb
Andrew Murray <radarhere@users.noreply.github.com>
2023-07-26 08:08:17 +1000

    Do not specify default mode

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open("README.rst", "r", encoding="utf-8") as fh:
+with open("README.rst", encoding="utf-8") as fh:
     long_description = fh.read()
 
 sane = setuptools.Extension('_sane',
