fe5fcf2ece647fbfb9f0241b643221469a05b437
Andrew Murray <radarhere@users.noreply.github.com>
2023-12-08 18:15:57 +1100

    Added pyproject.toml

--- setup.py
+++ setup.py
@@ -1,37 +1,14 @@
-import setuptools
+from setuptools import Extension, setup
 
-with open("README.rst", encoding="utf-8") as fh:
-    long_description = fh.read()
+ext_modules = [
+    Extension(
+        "_sane",
+        include_dirs=[],
+        libraries=["sane"],
+        define_macros=[],
+        extra_compile_args=[],
+        sources=["_sane.c"],
+    )
+]
 
-sane = setuptools.Extension('_sane',
-                 include_dirs=[],
-                 libraries=['sane'],
-                 define_macros=[],
-                 extra_compile_args=[],
-                 sources=['_sane.c'])
-
-setuptools.setup(name='python-sane',
-      version='2.9.1',
-      description='This is the python-sane package',
-      long_description=long_description,
-      long_description_content_type="text/x-rst",
-      url='https://github.com/python-pillow/Sane',
-      maintainer='Sandro Mani',
-      maintainer_email='manisandro@gmail.com',
-      packages=setuptools.find_packages(),
-      classifiers=[
-          "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.8",
-          "Programming Language :: Python :: 3.9",
-          "Programming Language :: Python :: 3.10",
-          "Programming Language :: Python :: 3.11",
-          "Programming Language :: Python :: 3.12",
-          "Programming Language :: Python :: 3 :: Only",
-          "Programming Language :: Python :: Implementation :: CPython",
-          "Programming Language :: Python :: Implementation :: PyPy",
-          "Topic :: Multimedia :: Graphics",
-          "Topic :: Multimedia :: Graphics :: Capture :: Scanners",
-      ],
-      python_requires=">=3.8",
-      py_modules=['sane'],
-      ext_modules=[sane])
+setup(ext_modules=ext_modules)
